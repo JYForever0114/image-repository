77ba4d31fb0c7bd329110bedacd939d2315110d1
jyf <1254023440@qq.com>
2025-04-17 15:00:38 +0700

    Feat: final commit

--- draft.sh
+++ draft.sh
@@ -1,7 +1,7 @@
-files=(docs/*.png)
+files=(docs/posemaniacs/*.png)
 step=500                # 每 500 张一批
 for ((i=0;i<${#files[@]}; i+=step)); do
     git add "${files[@]:i:step}"
     git commit -m "Batch $((i/step+1))"
-    git push origin main
+    git push
 done
